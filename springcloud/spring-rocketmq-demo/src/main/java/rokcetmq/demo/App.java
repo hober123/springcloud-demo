486cc0c10368afb50a6adbba7ee090a9fa8821a1
hober123 <xuehongbo_2010@163.com>
2019-07-11 18:12:32 +0800

    Create App.java

---
+++ springcloud/spring-rocketmq-demo/src/main/java/rokcetmq/demo/App.java
@@ -0,0 +1,17 @@
+package rokcetmq.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * rocketmq demo
+ * rocketmq 学习demo
+ */
+@SpringBootApplication
+public class App 
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(App.class, args);
+    }
+}
